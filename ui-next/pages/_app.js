fc21c051b507dd5ebad38767105275507d37e625
nezouse <stankiewicz1998@gmail.com>
2020-08-13 15:36:18 +0200

    Init next js project

---
+++ ui-next/pages/_app.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-unassigned-import
+import '../styles/globals.css';
+
+const MyApp = ({Component, pageProps}) => {
+	return <Component {...pageProps} />;
+};
+
+export default MyApp;
