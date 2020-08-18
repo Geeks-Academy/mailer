813966e52f639cde37bc24cb7e41cc9a472d7231
cosikowy <mr.cosik@gmail.com>
2020-08-18 20:05:11 +0200

    Celery and RabbitMQ added to docker compose and flask app

--- email/mjml_service/handler.py
+++ email/mjml_service/handler.py
@@ -6,22 +6,19 @@ from urllib.error import URLError
 import socket
 import requests
 
-class MJMLHandler:
-    def __init__(self, ip):
-        self.ip = ip
 
-    def convert(self, template_name='beforeLiveCodeReview'):
-        data = {
-            'templateName':template_name,
-            'templateVariables': {'title':'TYTUŁ POSTA'}
-        }
-        MJML_IP = f'http://{self.ip}:3000/convert'
-        try:
-            resp = requests.post(MJML_IP,  json=data)
-            return resp.json()
+def MJMLHandler_converter(ip, template_name='beforeLiveCodeReview'):
+    data = {
+        'templateName':template_name,
+        'templateVariables': {'title':'TYTUŁ POSTA'}
+    }
+    MJML_IP = f'http://{ip}:3000/convert'
+    try:
+        resp = requests.post(MJML_IP,  json=data)
+        return resp.json()
 
-        except requests.exceptions.RequestException as e:  
-            return {'error':str(e)}
+    except requests.exceptions.RequestException as e:  
+        return {'error':str(e)}
 
     # def templates(self, templateName):
     
